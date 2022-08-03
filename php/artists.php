4395c9bc5fcedc9959908cf43d8632d5073ba1b9
Shdorsh <am.bre@outlook.de>
2022-08-03 16:08:35 +0200

    Artist fetch php working even better
    You can never have enough "better"s

--- php/artists.php
+++ php/artists.php
@@ -1,14 +1,15 @@
 <?php
-    function searchSong() {
+    function searchSong($input) {
         // If submit wasn't pressed or someone pressed it wthout entering anything in the searchbar, return
-        if(!isset($_POST["submit"]) || !isset($_POST["artistSearch"]))
+        if(!isset($_POST["submit"]))
             return null;
         
         // Connect to the database and search for the artist and his genres
         $dbConnection = mysqli_connect("localhost","root",null,"spotify_db");
         $query = "SELECT artists.*, categories.title FROM artists
         INNER JOIN songs ON songs.artist_id = artists.id
-        INNER JOIN categories ON songs.categ_id = categories.id";
+        INNER JOIN categories ON songs.categ_id = categories.id
+        WHERE artists.name LIKE '%$input%'";
 
         // This is the data we got. it got multiple entries in case a certain artist has 2 genres
         $fetchAll = mysqli_fetch_all(mysqli_query($dbConnection, $query),MYSQLI_ASSOC);
@@ -35,18 +36,14 @@
         return $returnData;
     }
 
+    // Creates the divcard for each artist
     function createCard($name, $songNum, $genre, $bio) {
         echo "<div class='card-container'>
                 <div class='card-title'>
                     <div>" . $name . "</div>
                     <div>(" . $songNum . ")</div>
                 </div>
-                <div>";
-        foreach($genre as $category) {
-            echo $category . ", ";
-        }
-
-        echo "</div>
+                <div>" . implode(", ", array_unique($genre)) . "</div>
             <div>" . $bio . "...</div>
         </div>";
     }
@@ -59,6 +56,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../php/reset.css">
+    <link rel="stylesheet" href="../php/style.css">
 </head>
 <body> 
     <form method="post">
@@ -66,8 +65,14 @@
     </form>
 
     <?php
-        $returnArray = searchSong();
+        // Search for a given artist. The default name is an empty string, unless someone types in a name
+        $searchArtistName = "";
+        if(isset($_POST['artistSearch'])) {
+            $searchArtistName = $_POST['artistSearch'];
+        }
+        $returnArray = searchSong($searchArtistName);
 
+        // If the return array isn't empty, show the divcards. might add a message when nothing found
         if($returnArray) {
             foreach($returnArray as $artist)
                 createCard($artist['name'],$artist['songsnum'],$artist['genre'],$artist['bio']);
