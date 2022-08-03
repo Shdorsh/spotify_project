cb2d9f693236d1ec5f4028e361b068cf4daaa3fd
Shdorsh <am.bre@outlook.de>
2022-08-03 15:02:34 +0200

    Added the artist fetch

--- php/artists.php
+++ php/artists.php
@@ -1,3 +1,56 @@
+<?php
+    function searchSong() {
+        // If submit wasn't pressed or someone pressed it wthout entering anything in the searchbar, return
+        if(!isset($_POST["submit"]) || !isset($_POST["artistSearch"]))
+            return null;
+        
+        // Connect to the database and search for the artist and his genres
+        $dbConnection = mysqli_connect("localhost","root",null,"spotify_db");
+        $query = "SELECT artists.*, categories.title FROM artists
+        INNER JOIN songs ON songs.artist_id = artists.id
+        INNER JOIN categories ON songs.categ_id = categories.id";
+
+        // This is the data we got. it got multiple entries in case a certain artist has 2 genres
+        $fetchAll = mysqli_fetch_all(mysqli_query($dbConnection, $query),MYSQLI_ASSOC);
+
+        $returnData = array();
+
+        foreach($fetchAll as $value) {
+            $returnData[$value['name']] = array();
+            $returnData[$value['name']]['name'] = $value['name'];
+            $returnData[$value['name']]['bio'] = substr($value['bio'],0,20);
+
+            // Adding a genre
+            $returnData[$value['name']]['genre'][] = $value['title'];
+
+            // BUGGED needs to increment by 1
+            if(isset($returnData[$value['name']]['songsnum'])) {
+                $returnData[$value['name']]['songsnum']++;
+            } else {
+                $returnData[$value['name']]['songsnum'] = 1;
+            }
+        }
+
+        return $returnData;
+    }
+
+    function createCard($name, $songNum, $genre, $bio) {
+        echo "<div class='card-container'>
+                <div class='card-title'>
+                    <div>" . $name . "</div>
+                    <div>(" . $songNum . ")</div>
+                </div>
+                <div>";
+        foreach($genre as $category) {
+            echo $category . ", ";
+        }
+
+        echo "</div>
+            <div>" . $bio . "...</div>
+        </div>";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +59,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<body> 
+    <form method="post">
+        <input type="text" name="artistSearch" id="" /><input type="submit" name="submit" value="Search songs" />
+    </form>
+
+    <?php
+        $returnArray = searchSong();
+
+        if($returnArray) {
+            foreach($returnArray as $artist)
+                createCard($artist['name'],$artist['songsnum'],$artist['genre'],$artist['bio']);
+        }
+    ?>
 </body>
 </html>
