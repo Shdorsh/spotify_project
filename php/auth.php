ad16137c14e9c99908de85538ba4e7cb7f950638
Margo <106470282+grasser26@users.noreply.github.com>
2022-08-03 16:27:53 +0200

    Adding css

--- php/auth.php
+++ php/auth.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="/css/favicon-tv.png" />
     <link rel="stylesheet" href="/css/reset.css" />
+    <link rel="stylesheet" href="/spotify_project/css/style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100;1,400;1,700&display=swap" rel="stylesheet" />
